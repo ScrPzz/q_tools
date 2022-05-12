cd6c3509ae4df4992b63fba6f529c5210a97c802
ScrPzz <alessandrotogni1985@gmail.com>
2022-05-12 11:09:35 +0200

    [UPD] Tree structure fixex [ADD] Qt functions

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Personal set of data analysis tools.
 run: python setup.py bdist_wheel
 
 ## To install:
+cd ./dist
 run: pip install atogni_tools-0.1.5-py3-none-any.whl
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='atogni_tools',
-    packages=find_packages(include=['utilities']),
-    version='0.1.9',
+    packages=find_packages(include=['dict_tools', 'misc_tools', 'plot_tools', 'quant_tools']),
+    version='0.2.0',
     description='Analysis tools',
     author='Alessandro Togni',
     license='MIT',
